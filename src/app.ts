ec7263e9b4f1af2aa7fd19baf74a178dcd0d90b7
mrkvon <mrkvon@protonmail.com>
2022-09-20 18:57:18 +0300

    Make generic proxy for json api

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,47 @@
-import express from 'express'
+import express, { json } from 'express'
 import cors from 'cors'
 import axios from 'axios'
 import helmet from 'helmet'
+import type { Method } from 'axios'
 // import { addColors } from 'winston/lib/winston/config'
 // import debug from 'debug';
 
 // const log = debug('app')
 
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(helmet())
 app.use(cors())
+app.use(json())
 
-app.get(
+app.all(
   '/',
   async (req: express.Request<{}, {}, {}, { uri: string }>, res, next) => {
     try {
       if (req.query.uri) {
-        const response = await axios.get(req.query.uri, {
-          headers: { accept: req.headers?.accept ?? 'text/turtle' },
-        })
-        return res
-          .set(response.headers)
-          .status(response.status)
-          .send(response.data)
+        try {
+          const response = await axios.request({
+            url: req.query.uri,
+            method: req.method as Method,
+            data: req.body,
+            headers: { ...(req.headers.authorization ? { authorization: req.headers.authorization } : {}), accept: req.headers?.accept ?? 'text/turtle' },
+          })
+          return res
+            .set(response.headers)
+            .status(response.status)
+            .send(response.data)
+
+        }
+         catch (error) {
+          if (axios.isAxiosError(error) && error.response) {
+            return res
+              .set(error.response.headers)
+              .status(error.response.status)
+              .send(error.response.data)
+          } else throw error
+          
+        }
       } else {
         throw new Error('uri not specified')
       }
